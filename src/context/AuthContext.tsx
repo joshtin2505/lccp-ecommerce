1822e7eb3ec38bc0a5d9cecd49b061d72cf8bdcd
joshtin2505 <justincastro2505@gmail.com>
2024-03-28 14:40:56 -0500

    Refactor AuthContext to include allowedRole and isAuthenticated

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,19 +1,37 @@
 import { ReactNode, createContext, useEffect, useState } from "react"
 import { loginApi, verifyToken } from "@/api/users.api"
-import type { AxiosLoginResponse, LoginUserForm } from "@/types/extended.types"
+import type {
+  AxiosLoginResponse,
+  AxiosVerifyResponse,
+  LoginUserForm,
+} from "@/types/extended.types"
 import { AxiosError } from "axios"
-import { LoginRejectType } from "@/types/response.types"
+import { LoginRejectType, VerifyRejectType } from "@/types/response.types"
 import Cookies from "js-cookie"
+import { RolesType } from "@/types/users.types"
 type AuthRes =
   | AxiosLoginResponse
   | AxiosError<LoginRejectType, any>["response"]
+  | AxiosVerifyResponse
+  | AxiosError<VerifyRejectType, any>["response"]
   | null
-interface AuthContextValue {
+
+interface AuthContextStates {
   response: AuthRes
+  allowedRole: RolesType | null
+  isAuthenticated: boolean
+  loading: boolean
+}
+interface AuthContextActions {
   login: (data: LoginUserForm) => Promise<void>
 }
-export const AuthContext = createContext<AuthContextValue>({
+export const AuthContext = createContext<
+  AuthContextStates & AuthContextActions
+>({
   response: null,
+  allowedRole: null,
+  isAuthenticated: false,
+  loading: true,
   login: async (data: LoginUserForm) => {},
 })
 const { Provider } = AuthContext
@@ -21,8 +39,7 @@ const { Provider } = AuthContext
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [response, setResponse] = useState<AuthRes>(null)
   const [isAuthenticated, setIsAuthenticated] = useState(false)
-  // Cambiar tipo a el enum de UserRoles de user.constants.ts
-  const [isAllowed, setIsAllowed] = useState<number | null>(null)
+  const [allowedRole, setAllowedRole] = useState<RolesType | null>(null)
   const [loading, setLoading] = useState(false)
 
   const login = async (data: LoginUserForm) => {
@@ -32,45 +49,51 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       })
       .catch((err: AxiosError<LoginRejectType>) => {
         setResponse(err.response)
-        // console.log(typeof err.request)
       })
   }
 
   useEffect(() => {
     async function checkLogin() {
       const cookies = Cookies.get()
+
       if (!cookies.token) {
         setIsAuthenticated(false)
         setLoading(false)
         setResponse(null)
         return
       }
-      try {
-        const res = await verifyToken(cookies.token)
-        if (!res.data) {
+
+      verifyToken()
+        .then((res) => {
+          if (!res.data) {
+            setIsAuthenticated(false)
+            setLoading(false)
+            return
+          }
+          if (res.data.user.role === "admin") setAllowedRole("admin")
+          else if (res.data.user.role === "masterAdmin")
+            setAllowedRole("masterAdmin")
+          else if (res.data.user.role === "user") setAllowedRole("user")
+          setIsAuthenticated(true)
+          setResponse(res)
+          setLoading(false)
+        })
+        .catch((err: AxiosError<VerifyRejectType>) => {
+          setResponse(err.response)
           setIsAuthenticated(false)
           setLoading(false)
-          return
-        }
-        if (res.data.roll === 0) setIsAllowed(0)
-        else if (res.data.roll === 1) setIsAllowed(1)
-        else if (res.data.roll === 2) setIsAllowed(2)
-        else if (res.data.roll === 3) setIsAllowed(3)
-        setIsAuthenticated(true)
-        setResponse(res.data)
-        setLoading(false)
-      } catch (error) {
-        setIsAuthenticated(false)
-        setResponse(null)
-        setLoading(false)
-      }
+        })
     }
+    checkLogin()
   }, [])
   return (
     <Provider
       value={{
         response,
         login,
+        isAuthenticated,
+        allowedRole,
+        loading,
       }}
     >
       {children}
